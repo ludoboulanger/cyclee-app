c6fea22620dcd92b9488dcde8141928f380067ca
Ludovic Boulanger <ludovic.boulanger@outlook.com>
2022-01-30 12:16:52 -0500

    Spring boilerplate

---
+++ src/test/java/com/cyclee/app/CycleeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyclee.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
