7aea7f312332ab505fcb2093d4c6206301871188
Ludovic Boulanger <ludovic.boulanger@outlook.com>
2022-02-05 13:21:51 -0500

    Added eslint to typescript and formatting to java

--- backend/src/main/java/com/cyclee/api/CycleeApiApplication.java
+++ backend/src/main/java/com/cyclee/api/CycleeApiApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CycleeApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CycleeApiApplication.class, args);
+  public void test(int a, int b) {
+    if (a == b) {
+      System.out.println("True");
+    } else {
+      System.out.println("False");
     }
+
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(CycleeApiApplication.class, args);
+  }
 
 }
