a9ac9fb9e189a41383fcd4a580e07e0dfc2a304c
Ludovic Boulanger <ludovic.boulanger@outlook.com>
2022-02-05 13:25:54 -0500

    Removed useless changes

--- backend/src/main/java/com/cyclee/api/CycleeApiApplication.java
+++ backend/src/main/java/com/cyclee/api/CycleeApiApplication.java
@@ -6,15 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CycleeApiApplication {
 
-  public void test(int a, int b) {
-    if (a == b) {
-      System.out.println("True");
-    } else {
-      System.out.println("False");
-    }
-
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(CycleeApiApplication.class, args);
   }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,11 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const test = (big:number) => {
-    console.log("Hpli");
-  };
-
   return <Component {...pageProps} />;
 }
 
