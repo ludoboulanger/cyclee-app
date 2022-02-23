9a4d51521f3c968ac0beca3e381efa7ba30cbd91
vfreve <44242537+VicoF@users.noreply.github.com>
2022-02-22 20:19:24 -0500

    remove router hooks that are not useful anymore

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import type { AppProps } from "next/app";
 import { CacheProvider } from "@emotion/react";
 import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
@@ -11,27 +11,12 @@ import lightThemeOptions from "../styles/theme/lightThemeOptions";
 import "../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import "../firebase.config";
-import { useRouter } from "next/router";
 
 const clientSideEmotionCache = createEmotionCache();
 
 const lightTheme = createTheme(lightThemeOptions);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const handleStart = (url: String) => {
-      console.log(`Loading ${url}`);
-      setLoading(true);
-    };
-    const handleComplete = () => setLoading(false);
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-  }, [router]);
-
   return (
     <CacheProvider value={clientSideEmotionCache}>
       <ThemeProvider theme={lightTheme}>
